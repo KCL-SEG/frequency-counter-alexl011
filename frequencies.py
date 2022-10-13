ede4fecfcb40a092355f4c87479a756f75445e20
Shih-Hung Lin <shih-hung.lin@kcl.ac.uk>
2022-10-13 14:42:16 +0100

    add initial commit

--- frequencies.py
+++ frequencies.py
@@ -3,5 +3,11 @@
 
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
+    for x in items:
+        x = str(x)
+        if x in frequencies:
+            frequencies[x] += 1
+        else:
+            frequencies[x] = 1
+
     return frequencies
